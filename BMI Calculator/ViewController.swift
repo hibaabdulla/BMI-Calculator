1dee69579e6c51a46225f4067be1c6b886bc7bdf
Hiba Abdulla <hibaabdullahkk@gmail.com>
2025-05-23 17:30:09 -0700

    Display Height and Weight

--- BMI Calculator/ViewController.swift
+++ BMI Calculator/ViewController.swift
@@ -8,20 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    
+    @IBOutlet weak var weightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func heightSliderChanged(_ sender: UISlider) {
-        var height = sender.value
-        print(String(format: "%.2f", height))
+        heightLabel.text = "\(String(format: "%.2f", sender.value)) m"
     }
     
     @IBAction func weightSliderChanged(_ sender: UISlider) {
-        var weight = sender.value
-        print(String(format: "%.2f", weight))
+        weightLabel.text = "\(String(format: "%.2f", sender.value)) kg"
     }
 }
 
