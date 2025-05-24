c33c5578cd29e60900be49bde3e4677e344d9340
Hiba Abdulla <hibaabdullahkk@gmail.com>
2025-05-23 17:19:41 -0700

    Print Slider Value

--- BMI Calculator/ViewController.swift
+++ BMI Calculator/ViewController.swift
@@ -14,6 +14,14 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        var height = sender.value
+        print(String(format: "%.2f", height))
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        var weight = sender.value
+        print(String(format: "%.2f", weight))
+    }
 }
 
